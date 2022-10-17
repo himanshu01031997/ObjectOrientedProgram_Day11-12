7613e80f1fb107f6ad2fa78a63395346610098d8
himanshu01031997 <hs010397@gmail.com>
2022-10-17 19:08:22 +0530

    stock management

--- Inventory.cs
+++ Inventory.cs
@@ -8,35 +8,14 @@ namespace InventoryManagement
 {
     public class Inventory//model class
     {
-        public List <TypeofRice> typeofRice;
-       
-        public List <TypeofWheat> typeofWheat;
-
-        public List <TypeofPulse> typeofPulse;
-
-
-
+        public List <StockAccount> stockAccount;
+ 
     }
-    public class TypeofRice
+    public class StockAccount
     {
-        public string name { get; set; }
-        public int weight { get; set; }
-        public int price { get; set; }
+        public string stockname { get; set; }
+        public int ShareNo { get; set; }
+        public int Shareprice { get; set; }
 
     }
-    public class TypeofWheat
-    {
-        public string name { get; set; }
-        public int weight { get; set; }
-        public int price { get; set; }
-
-    }
-    public class TypeofPulse
-    {
-        public string name { get; set; }
-        public int weight { get; set; }
-        public int price { get; set; }
-
-    }
-
 }
--- Program.cs
+++ Program.cs
@@ -7,33 +7,19 @@
             string path = "D:\\RFP-208\\InventoryManagement\\InventoryMan\\Inventory.json";
             FetchingInventoryDetails fetchingInventoryDetails = new FetchingInventoryDetails();
             Inventory data = fetchingInventoryDetails.Read(path);
-            Console.WriteLine("types of rice");
-            for (int i = 0; i < data.typeofRice.Count; i++)
+            Console.WriteLine("Stock Account Management");
+            int totalStockValue = 0;
+            for (int i = 0; i < data.stockAccount.Count; i++)
             {
-                Console.WriteLine(data.typeofRice[i].name);
-                Console.WriteLine(data.typeofRice[i].weight);
-                Console.WriteLine(data.typeofRice[i].price);
-                int val = data.typeofRice[i].weight * data.typeofRice[i].price;
-                Console.WriteLine("the price for {0}kg of type {1} is {2}", data.typeofRice[i].weight, data.typeofRice[i].name, val);
+                Console.WriteLine("Share Name:" + data.stockAccount[i].stockname);
+                Console.WriteLine("Share Number:" + data.stockAccount[i].ShareNo);
+                Console.WriteLine("Share Price:" + data.stockAccount[i].Shareprice);
+                int stockvalue = data.stockAccount[i].ShareNo * data.stockAccount[i].Shareprice;
+                Console.WriteLine("Total Stock Value of {0} is :{1}",data.stockAccount[i].stockname, stockvalue);
+                totalStockValue+=stockvalue;
+                Console.WriteLine("Total value of all Stocks are:{0}",totalStockValue);
             }
-            Console.WriteLine("types of wheat");
-            for (int i = 0; i < data.typeofWheat.Count; i++)
-            {
-                Console.WriteLine(data.typeofWheat[i].name);
-                Console.WriteLine(data.typeofWheat[i].weight);
-                Console.WriteLine(data.typeofWheat[i].price);
-                int val1 = data.typeofWheat[i].weight * data.typeofWheat[i].price;
-                Console.WriteLine("the price for {0}kg of type {1} is {2}", data.typeofWheat[i].weight, data.typeofWheat[i].name, val1);
-            }
-            Console.WriteLine("types of pulse");
-            for (int i = 0; i < data.typeofPulse.Count; i++)
-            {
-                Console.WriteLine(data.typeofPulse[i].name);
-                Console.WriteLine(data.typeofPulse[i].weight);
-                Console.WriteLine(data.typeofPulse[i].price);
-                int val2 = data.typeofPulse[i].weight * data.typeofPulse[i].price;
-                Console.WriteLine("the price for {0}kg of type {1} is {2}", data.typeofPulse[i].weight, data.typeofPulse[i].name, val2);
-            }
+
         }
     } 
 }
